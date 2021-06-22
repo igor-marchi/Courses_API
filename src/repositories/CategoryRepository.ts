3bbc70580e3a0a496f4d81896435bc0fe6706344
Igorstoso <igoraugustomarchi@gmail.com>
2021-06-21 21:19:59 -0300

    starting

---
+++ src/repositories/CategoryRepository.ts
@@ -0,0 +1,34 @@
+import { Category } from '../model/Category';
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoryRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    return this.categories.find((category) => category.name === name);
+  }
+}
+export { CategoryRepository };
---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { CategoryRepository } from '../repositories/CategoryRepository';
+
+const categoriesRoutes = Router();
+const categoryRepository = new CategoryRepository();
+
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoryRepository.list();
+
+  return response.status(200).json(all);
+});
+
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  const categoryAlreadyExists = categoryRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category Already exists' });
+  }
+
+  categoryRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+export { categoriesRoutes };
