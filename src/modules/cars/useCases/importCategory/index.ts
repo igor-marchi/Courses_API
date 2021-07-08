5000ef4aa399e16232e7a1e4c1fbd24906220be6
Igorstoso <igoraugustomarchi@gmail.com>
2021-07-07 21:56:26 -0300

    criado todo useCase para ler arquivos

---
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -0,0 +1,11 @@
+import { CategoryRepository } from "../../repositories/implementations/CategoryRepository";
+import { ImportCategoryController } from "./ImportCategoryController";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+const categoryRepository = CategoryRepository.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoryRepository);
+const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase
+);
+
+export { importCategoryController };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,12 @@
+import multer from "multer";
+
 import { Router } from "express";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoryController } from "../modules/cars/useCases/listCategory";
-
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 const categoriesRoutes = Router();
+
+const upload = multer({ dest: "./tmp" });
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -10,6 +14,10 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoryController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
