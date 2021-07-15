ace8eb00fa0e9a9c82a4a708702cf43a6214491a
Igorstoso <igoraugustomarchi@gmail.com>
2021-07-14 22:17:01 -0300

    Criada documentação com swagger

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -2,12 +2,12 @@ import { Request, Response } from "express";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 class CreateCategoryController {
-  constructor(private createCategtoryUseCase: CreateCategoryUseCase) {}
+  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
   handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
 
-    this.createCategtoryUseCase.execute({ name, description });
+    this.createCategoryUseCase.execute({ name, description });
 
     return response.status(201).send();
   }
